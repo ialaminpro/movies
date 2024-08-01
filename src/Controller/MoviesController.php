56c982402151386ef48fba25b2db521dde81418c
Al Amin <ialamin.pro@gmail.com>
2024-08-01 19:00:54 +0200

    Design and display movies list page

--- src/Controller/MoviesController.php
+++ src/Controller/MoviesController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Movie;
+use App\Repository\MovieRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,12 +11,17 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class MoviesController extends AbstractController
 {
-    #[Route('/movies', name: 'movies')]
-    public function index(EntityManagerInterface $em): Response
+    private $movieRepository;
+    public function __construct(MovieRepository $movieRepository)
     {
-        $repository = $em->getRepository(Movie::class);
-        $movies = $repository->findAll();
+        $this->movieRepository = $movieRepository;
+    }
 
-        return $this->render('index.html.twig',['movies' => $movies]);
+    #[Route('/movies', name: 'movies')]
+    public function index(): Response
+    {
+        $movies = $this->movieRepository->findAll();
+
+        return $this->render('movies/index.html.twig',['movies' => $movies]);
     }
 }
