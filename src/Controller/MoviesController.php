107135afb22601d908dc1e04b9d0368cd253bf9b
Al Amin <ialamin.pro@gmail.com>
2024-08-01 12:59:52 +0200

    Initiated twig template and display movie list

--- src/Controller/MoviesController.php
+++ src/Controller/MoviesController.php
@@ -3,17 +3,16 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MoviesController extends AbstractController
 {
-    #[Route('/movies', name: 'app_movies')]
-    public function index(): JsonResponse
+    #[Route('/movies', name: 'movies')]
+    public function index(): Response
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/MoviesController.php',
-        ]);
+        $movies = ['Inception', 'Black Window', 'Dark', 'Titanic', 'Avengers'];
+
+        return $this->render('index.html.twig', array( 'movies' => $movies));
     }
 }
