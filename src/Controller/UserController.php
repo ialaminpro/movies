5eb94a673533bba9c18083f8f60568d979c829f4
Al Amin <ialamin.pro@gmail.com>
2024-08-21 18:04:03 +0200

    Wildcard search using elastic search

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,18 +2,21 @@
 
 namespace App\Controller;
 
-use App\Document\Rental;
-use App\Form\RentalType;
 use App\Form\UserType;
 use Elastica\Query\MatchQuery;
+use Elastica\Suggest;
+use Elastica\Suggest\Completion;
 use FOS\ElasticaBundle\Finder\TransformedFinder;
 use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\ODM\MongoDB\DocumentManager;
 use App\Document\User;
+use Elastica\Search;
+use Elastica\Query;
 
 
 class UserController extends AbstractController
@@ -98,20 +101,30 @@ class UserController extends AbstractController
         ]);
     }
 
-    #[Route('/users/search/{email}', methods:['GET'], name: 'user_search')]
-    public function search($email)
+    #[Route('/users/search/suggest', methods:['GET'], name: 'user_search_suggest')]
+    public function suggest(Request $request): JsonResponse
     {
+        $query = $request->query->get('q', '');
 
-        $query = new MatchQuery();
-        $query->setFieldQuery('email', $email);
+        $queryString = '*' . $query . '*';
+        $searchParams = [
+            'query' => [
+                'query_string' => [
+                    'query' => $queryString,
+                    'fields' => ['firstName', 'lastName']
+                ]
+            ]
+        ];
 
-        $results = $this->_userFinder->find($query);
+        // Perform the search
+        $results = $this->_userFinder->find($searchParams);
 
-        return $this->render('user/search.html.twig', [
-            'results' => $results,
+         return new JsonResponse([
+            'suggestions' => array_map(function($item) {
+                return ['firstName' => $item->getFirstName() . ' '. $item->getLastName()];
+            }, $results),
         ]);
     }
-
     #[Route('/users/show/{id}', methods:['GET'], name: 'user_detail')]
     public function show($id)
     {
