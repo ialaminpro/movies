24c03bacdfdc2e1ad8cd7133c01f62813eb06b4a
Al Amin <ialamin.pro@gmail.com>
2026-04-20 11:16:00 +0200

    build: upgrade the supported Symfony and PHP platform

--- config/bundles.php
+++ config/bundles.php
@@ -11,5 +11,5 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
-    Doctrine\Bundle\MongoDBBundle\DoctrineMongoDBBundle::class => ['all' => true]
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 ];
